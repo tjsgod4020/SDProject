a70a616d9559999182d656cb2ebd1c0b576ab9a8
tjsgod4020 <tjsgod4020@gmail.com>
2025-10-17 21:27:08 +0900

    chore: remove old SDProject folder (greenfield start)

--- Assets/SDProject/Scripts/Combat/Board/TeamSide.cs
+++
@@ -1,5 +0,0 @@
-// Assets/SDProject/Scripts/Combat/Board/TeamSide.cs
-namespace SDProject.Combat.Board
-{
-    public enum TeamSide { Ally, Enemy }
-}
--- Assets/SDProject/Scripts/Core/FSM/IState.cs
+++
@@ -1,9 +0,0 @@
-namespace SDProject.Core.FSM
-{
-    public interface IState
-    {
-        void Enter();
-        void Tick(float dt);
-        void Exit();
-    }
-}
